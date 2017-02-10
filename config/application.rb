c7045b0fcfbe8201e63683cfd2217dc03073d0ea
Carl Molinari <cmolinari@newrelic.com>
2017-02-10 11:47:55 -0800

    Removing Rack::Lock

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module NewrelicRubyKata
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.middleware.delete Rack::Runtime
+    config.middleware.delete Rack::Lock
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
