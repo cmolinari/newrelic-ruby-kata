b724dfc8dd4308b22355baf32143e73e1cbf5649
Carl Molinari <cmolinari@newrelic.com>
2017-02-10 11:45:22 -0800

    delete Rack::Runtime middleware

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module NewrelicRubyKata
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.middleware.delete Rack::Runtime
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
